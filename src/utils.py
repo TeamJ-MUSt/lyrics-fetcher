b04549a9b6d0cb9f90fae707ebef27e8fd29e93c
HappyFaceFriend <yjl980730@gmail.com>
2024-04-08 14:31:33 +0900

    Unnecessary newline and return characters are removed from lyrics

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ def remove_korean_lines(input_string):
     korean_pattern = re.compile("[\uAC00-\uD7A3]+")
 
     lines = input_string.split('\n')
-    filtered_lines = [line.strip() for line in lines if not korean_pattern.search(line)]
+    filtered_lines = [line.strip().replace('\n', '').replace('\r', '') for line in lines if not korean_pattern.search(line)]
     filtered_string = '\n'.join(filtered_lines)
-    return filtered_string
+    cleaned_string = re.sub(r'\n+', '\n', filtered_string)
+    return cleaned_string
